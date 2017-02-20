93e433f4a827a532b28d4afc6941f0dd3996a2e6
sasquad92 <krzysztof.sommerrey@gmail.com>
2017-02-20 18:47:06 +0100

    still...

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"os"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/login", LoginHandler)
 
 	fmt.Println("Starting server...")
-	http.ListenAndServe(":5000", r)
+	http.ListenAndServe(":"+port, r)
 }
